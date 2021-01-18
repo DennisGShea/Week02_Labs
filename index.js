1a3e56561355b394a586190340a8ecaa49eaa9ad
Dennis G Shea <dgs@Denniss-MacBook-Pro.local>
2021-01-18 10:26:09 -0500

    wk2

---
+++ index.js
@@ -0,0 +1,9 @@
+// start of week two !!
+
+// REMEMBER first time 
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/DennisGShea/Week02_Labs.git
+// git push -u origin main
